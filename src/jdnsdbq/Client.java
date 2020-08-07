fb9bd21e7ce67e961e99b245040ef7f6cccf54fe
mwait101101 <59382740+mwait101101@users.noreply.github.com>
2020-08-07 15:58:43 -0400

    improved readability, added manual API key input option if config's not valid
    rewrote/reorganized most of the program to improve readability, deleted unnecessary files, and added the ability to manually input your API key if there is not a valid API key in the config file.

--- src/jdnsdbq/Client.java
+++ src/jdnsdbq/Client.java
@@ -13,60 +13,127 @@ import java.io.Reader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Optional;
+import java.util.Scanner;
+import java.net.MalformedURLException;
 //import java.net.HttpURLConnection;
 class Client {
   static jLog jlog = new jLog();
   static String APIroot = "https://api.dnsdb.info/";
+  static Scanner uin = new Scanner(System.in);
+  static String API_key;
+  static File configFile;
   public static void main(String[] args) {
-    //jlog.ls("Starting program...");
     jlog.ls("Starting program (Client.java)");
-    //
-    File configFile = new File("config.txt");
-
+    configFile = new File("config.txt");
     jlog.ls("created new File object (configFile)");
-    try {
-                FileParser fp = new FileParser(configFile);
-      Optional<String> OpAPI_Key = fp.getValue("API_Key");
-      //fp.close();
-      if(OpAPI_Key.isPresent()) {
-        String API_Key = OpAPI_Key.get();
-        System.out.println("Your API Key is " + API_Key);
-        try {
-          String r = (testAPIKey(API_Key)) ? "api key validated successfully" : "invalid api key";
-          jlog.ls(r);
-          r = null;
-        }
-        catch(Exception e) {
-          System.out.println(":( " + e);
-        }
-      }
-      else {
-        System.out.println("API Key was not found.");
-        //TODO ask user if they want to enter their API key here manually
-      }
-    }
-    catch(FileNotFoundException e) {
-      System.out.println(e);
-    }
+      doAPIkey();
+
     jlog.ls("end of program");
   }
 
-  static boolean testAPIKey(String APIKey) throws Exception {
+  static void doAPIkey() {
+    if (APIkeyFoundInFile(configFile) == true) {
+      System.out.println("Successfully retrieved API key with value " + API_key + " from the config file.");
+      try {
+        if (testAPIKey(API_key) == true) {
+          System.out.println("Your API key is valid!");
+          return;
+        }
+        else {
+          System.out.println("Your API key is invalid.");
+        }
+      }
+      catch(IOException ex) {
+        System.out.println(ex);
+        //throw ex;
+      }
+    }
+    else {
+      //TODO change it so the program won't input the message below if the config file could not be found --maybe use a Boolean instead of boolean for the APIkeyFoundInFile method to accomplish this?
+      System.out.println("Unfortunately, an API key could not be found in the config file.");
+    }
+      doManualAPIkeyInput();
+  }
+
+  //
+  static boolean APIkeyFoundInFile(File configFile) {
+    FileParser fp = new FileParser(configFile);
+    try {
+      Optional<String> OpAPI_Key = fp.getValue("API_Key");
+      if(OpAPI_Key.isPresent()) {
+        API_key = OpAPI_Key.get();
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    catch(FileNotFoundException e) {
+      System.out.println("Could not find the config file.");
+      //TODO add option to create the config  file now or specify the new location of the File
+      return false;
+    }
+  }
+
+  static void doManualAPIkeyInput() {
+    System.out.println("Please enter your API key below:");
+    Scanner sc = new Scanner(System.in);
+    API_key = sc.nextLine();
+    System.out.println("Testing API key...");
+    try {
+      if (testAPIKey(API_key) == true ) {
+        System.out.println("Your API key is valid!");
+        saveAPIkeytoConfigFile();
+      }
+      else {
+        System.out.println("Your API key was invalid. Trying again...");
+        doManualAPIkeyInput();
+      }
+    }
+    catch(IOException ex) {
+      System.out.println(ex);
+      //throw ex;
+    }
+    finally {
+      sc.close();
+    }
+  }
+
+
+
+  static void saveAPIkeytoConfigFile() {
+    //TODO
+    System.out.println("saving API key to the config file...");
+  }
+
+
+
+  static boolean testAPIKey(String APIKey) throws IOException {
     jlog.ls("start of testAPIKey method");
-    URL url = new URL(APIroot + "/lookup");
-    HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
-    jlog.ls("connection opened");
-    conn.setRequestProperty("X-API-Key", APIKey);
-    //String data = String.valueOf(conn.getErrorStream()) ;
-//    String data = (conn.getResponseCode() < 299) ? String.valueOf(conn.getResponseMessage()) : String.valueOf(conn.getResponseMessage()+ "eee" + conn.getResponseCode());
-    //StringBuilder data;
-  //  conn.getHeaderFields().forEach(header -> data += header);
-  //  return data;
-      //String fullResponse = getFullResponse(conn);//data;
-      jlog.ls("end of testAPIKey method");
-      return (conn.getResponseCode() != HttpsURLConnection.HTTP_FORBIDDEN);
-    //return new String((HttpsURLConnection conn = (HttpsURLConnection)(new URL(APIroot))-> conn.openConnection() -> conn.getResponseCode() > 299) ? this.getResponseMessage() : this.getErrorStream()  );
-    // what the return statement is supposed to do is create a new URL from the String APIroot, then create an HttpsURLConnection from that, then open the connection, get the connection's response code, check if it that code is over 299, getting its response message if it is, and getting its error stream if it is not, and then finally converting the response's value to a string and then returning that string.
+    try {
+          URL url = new URL(APIroot + "/lookup");
+          HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
+          jlog.ls("connection opened");
+          conn.setRequestProperty("X-API-Key", APIKey);
+          //String data = String.valueOf(conn.getErrorStream()) ;
+      //    String data = (conn.getResponseCode() < 299) ? String.valueOf(conn.getResponseMessage()) : String.valueOf(conn.getResponseMessage()+ "eee" + conn.getResponseCode());
+          //StringBuilder data;
+        //  conn.getHeaderFields().forEach(header -> data += header);
+        //  return data;
+            //String fullResponse = getFullResponse(conn);//data;
+            jlog.ls("end of testAPIKey method");
+            return (conn.getResponseCode() != HttpsURLConnection.HTTP_FORBIDDEN);
+          //return new String((HttpsURLConnection conn = (HttpsURLConnection)(new URL(APIroot))-> conn.openConnection() -> conn.getResponseCode() > 299) ? this.getResponseMessage() : this.getErrorStream()  );
+          // what the return statement is supposed to do is create a new URL from the String APIroot, then create an HttpsURLConnection from that, then open the connection, get the connection's response code, check if it that code is over 299, getting its response message if it is, and getting its error stream if it is not, and then finally converting the response's value to a string and then returning that string.
+    }
+    catch(MalformedURLException ex) {
+      System.out.println(ex);
+      throw ex;
+    }
+    catch(IOException ex) {
+      System.out.println(ex);
+      throw ex;
+    }
   }
   public static String getFullResponse(HttpsURLConnection con) throws IOException {//TODO: add Https unique responses (currently it only has the http responses)
         StringBuilder fullResponseBuilder = new StringBuilder();
